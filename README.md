d8c909a55459701b4c27cf5156a491b30bcedd95
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-12-19 13:20:57 +0600

    ^

---
+++ README.md
@@ -0,0 +1,21 @@
+Welcome to the X-education! This server is designed by Syed Nazmul Hossain.
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/nazmulhossain17/x-education.git
+
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+    cd x-education
+   ```
+
+3. Install the project dependencies:
+   ```bash
+    npm install
+   ```
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -12,19 +12,27 @@ const {
 const { isLoggedOut, isAdmin, isLoggedIn } = require("../middleware/index");
 
 const router = express.Router();
-
+// testing route
 router.get("/", (req, res) => {
   res.send("2working");
 });
 
 // register user
 router.post("/register", isLoggedOut, processRegister);
+// login user
 router.post("/login", isLoggedOut, handleLogin);
+// log out user
 router.get("/logout", isLoggedIn, handleLogout);
 
+// create course
 router.post("/create-course", isLoggedIn, isAdmin, createCourse);
+// all course data
 router.get("/all-course", getAllCourses);
+// get single course data
 router.get("/all-course/:id", getSingleCourse);
+// update course
 router.patch("/update-course/:id", isLoggedIn, isAdmin, updateCourse);
+// delete course
 router.delete("/delete-course/:id", isLoggedIn, isAdmin, deleteCourse);
+
 module.exports = router;
